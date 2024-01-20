3edaa33eee96d32a58eacbf55a7abd1cabf8adee
chegun95 <62911253+chegun95@users.noreply.github.com>
2024-01-20 10:14:30 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -40,7 +40,53 @@ void write()
 			++counter;
 			cin >> a >> b;
 			cout << counter << ". ";
-		
+			{
+				if (arr[i] != 0) {
+					cout << "Проверка<><<<<><><";
+				}
+				else if (isvalid(arr[i]) &&
+					arr[0] == arr[1] && arr[1] == arr[2] ||
+					arr[0] == arr[4] && arr[4] == arr[8] ||
+					arr[0] == arr[3] && arr[3] == arr[6]) {
+
+					cout << "Победа<<<<0";
+					reset();
+					write();
+					break;
+				}
+				else if (isvalid(arr[i]) &&
+					arr[0] == arr[1] && arr[1] == arr[2] ||
+					arr[1] == arr[4] && arr[4] == arr[7]) {
+					cout << "Победа<<<<2";
+					reset();
+					write();
+					break;
+				}
+				else if (isvalid(arr[i]) && arr[0] == arr[1] && arr[1] == arr[2] ||
+					arr[2] == arr[4] && arr[4] == arr[6] ||
+					arr[2] == arr[5] && arr[5] == arr[8]) {
+					cout << "Победа<<<<3";
+					reset();
+					write();
+					break;
+				}
+
+				/*	1 123 159 147
+					2 123 258
+					3 123 357 369
+
+					4 147 456
+					5 456 258
+					6 456 369
+
+					7 147 753 789
+					8 789 258
+					9 789 159 369 */
+			}
+			reset();
+			write();
+			break;
+		}
 			
 			arr[a] = b;
 			display();
