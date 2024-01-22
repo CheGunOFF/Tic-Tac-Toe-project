747ba87fdfc893baaf75c71f91b28cf01625ba3b
chegun95 <62911253+chegun95@users.noreply.github.com>
2024-01-22 23:45:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,85 @@
 #include <iostream>
- /*
- - заменить 1 и 2 на x o
- - сделать вывод кто победил
- - сделать счёт побед
- - привести код в порядок!
- 
- 
- */
+#include <cstdlib>
+#include <string>
+
 using namespace std;
+ /*
 
-int arr[9];
+ 
+ - привести код в порядок!
 
+	- заменить 1 и 2 на x o - DONE
+	- сделать вывод кто победил - DONE
+	- сделать счёт побед - DONE
+
+ */
+
+/*
+-проверил массив на пустоту.
+-если пустой, то записать,
+-если полный,
+-то проверить следующий индекс
+-если пустой записать
+-ошибка
+*/
+
+
+
+char arr[9];
+
+bool isvalid();
+bool isGameStarted();
 void display();
 void reset();
-bool isvalid();
+void showScore();
+void win();
+void testWrite(string name, string score);
+void gameStart();
+void gameStop();
 
-bool isvalid(int a = 1, int b = 1)  // Возврат булевого фолс, если ноль
+bool gameStarted = false;
+string playerName;
+
+string scoreTable[10][3] = { {"Chegun", "121211544124584 :D"} };
+
+bool isvalid(int a = 1, char b = ' ')  // Возврат булевого фолс, если ноль | - chars
 {
-	if ((a >= 0 && a <= 9) && (b >= 1 && b <= 2)) {
+	if ((a >= 0 && a <= 9) && (b == 'x' || b == 'o')) {
 		return true;
 	}
 	else {
-		return false;
+
+		return false ;
 	}
 }
 
-
 // функция записи
-void write()
+void read()
 {
-	int a = 1;
-	int b = 1;
-	int counter = 0;
-
-	for (;;) {
+	int a;
+	char b;
+	
+	for (int i = 1;;++i) {
 		display();
-		++counter;
-		cout << counter << ". ";
+		gameStart();
+		string localScore = "1";
+		//cout << i << ". ";
 		cin >> a >> b;
+
+		if (cin.fail()) {  // проверка, является ли ввод числом
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+			cout << "It's not a number!" << endl;
+			read();
+			break;
+			
+		}
+
+
 		if (isvalid(a, b)) {    // если введённые числа истина то:
-			cout << "Верное значение..." << endl;
+
+			cout << "Valid character..." << endl;
 			arr[a] = b;
 
 			for (int i = a; i < 9; ++i) {
@@ -48,20 +88,28 @@ void write()
 					(isvalid(i, arr[i]) && (arr[i] == arr[0] && arr[i] == arr[4] && arr[i] == arr[8])) ||
 					(isvalid(i,arr[i]) && (arr[i] == arr[0] && arr[i] == arr[3] && arr[i] == arr[6])))
 				{
-					cout << "Победа<<<< 0";
 					display();
+
+					cout << "\n\n" << playerName << " - Winner !!!" << endl;
+					cout << "Take your " << localScore << " point!!!" << endl;
+
+					testWrite(playerName, localScore);
 					reset();
-					write();
+					read();
 					break;
 				}
 				else if (
 					(isvalid(i, arr[i]) && (arr[i] == arr[0] && arr[i] == arr[1] && arr[i] == arr[2])) ||
-					(isvalid(i, arr[i]) && (arr[i] == arr[1] && arr[i] == arr[7] && arr[i] == arr[7])))
+					(isvalid(i, arr[i]) && (arr[i] == arr[1] && arr[i] == arr[4] && arr[i] == arr[7])))
 				{
-					cout << "Победа<<<< 1";
 					display();
+
+					cout << "\n\n" << playerName << " - Winner !!!" << endl;
+					cout << "Take your " << localScore << " point!!!" << endl;
+
+					testWrite(playerName, localScore);
 					reset();
-					write();
+					read();
 					break;
 				}
 				else if (
@@ -69,40 +117,56 @@ void write()
 					(isvalid(i, arr[i]) && (arr[i] == arr[2] && arr[i] == arr[4] && arr[i] == arr[6])) ||
 					(isvalid(i, arr[i]) && (arr[i] == arr[2] && arr[i] == arr[5] && arr[i] == arr[8])))
 				{
-					cout << "Победа<<<< 2";
 					display();
+
+					cout << "\n\n" << playerName << " - Winner !!!" << endl;
+					cout << "Take your " << localScore << " point!!!" << endl;
+
+					testWrite(playerName, localScore);
 					reset();
-					write();
+					read();
 					break;
 				}
 				else if (
 					(isvalid(i, arr[i]) && (arr[i] == arr[0] && arr[i] == arr[3] && arr[i] == arr[6])) ||
 					(isvalid(i, arr[i]) && (arr[i] == arr[3] && arr[i] == arr[4] && arr[i] == arr[5])))
 				{
-					cout << "Победа<<<< 3";
 					display();
+				
+					cout << "\n\n" << playerName << " - Winner !!!" << endl;
+					cout << "Take your " << localScore << " point!!!" << endl;
+
+					testWrite(playerName, localScore);
 					reset();
-					write();
+					read();
 					break;
 				}
 				else if (
 					(isvalid(i, arr[i]) && (arr[i] == arr[3] && arr[i] == arr[4] && arr[i] == arr[5])) ||
 					(isvalid(i, arr[i]) && (arr[i] == arr[1] && arr[i] == arr[4] && arr[i] == arr[7])))
 				{
-					cout << "Победа<<<< 4";
 					display();
+					
+					cout << "\n\n" << playerName << " - Winner !!!" << endl;
+					cout << "Take your " << localScore << " point!!!" << endl;
+
+					testWrite(playerName, localScore);
 					reset();
-					write();
+					read();
 					break;
 				}
 				else if (
 					(isvalid(i, arr[i]) && (arr[i] == arr[3] && arr[i] == arr[4] && arr[i] == arr[5])) ||
 					(isvalid(i, arr[i]) && (arr[i] == arr[2] && arr[i] == arr[5] && arr[i] == arr[8])))
 				{
-					cout << "Победа<<<< 5";
 					display();
+
+					cout << "\n\n" << playerName << " - Winner !!!" << endl;
+					cout << "Take your " << localScore << " point!!!" << endl;
+
+					testWrite(playerName, localScore);
 					reset();
-					write();
+					read();
 					break;
 				}
 				else if (
@@ -110,20 +174,28 @@ void write()
 					(isvalid(i, arr[i]) && (arr[i] == arr[6] && arr[i] == arr[4] && arr[i] == arr[2])) ||
 					(isvalid(i, arr[i]) && (arr[i] == arr[6] && arr[i] == arr[7] && arr[i] == arr[8])))
 				{
-					cout << "Победа<<<< 6";
 					display();
+				
+					cout << "\n\n" << playerName << " - Winner !!!" << endl;
+					cout << "Take your " << localScore << " point!!!" << endl;
+
+					testWrite(playerName, localScore);
 					reset();
-					write();
+					read();
 					break;
 				}
 				else if (
 					(isvalid(i, arr[i]) && (arr[i] == arr[6] && arr[i] == arr[7] && arr[i] == arr[8])) ||
 					(isvalid(i, arr[i]) && (arr[i] == arr[1] && arr[i] == arr[4] && arr[i] == arr[7])))
 				{
-					cout << "Победа<<<< 7";
 					display();
+				
+					cout << "\n\n" << playerName << " - Winner !!!" << endl;
+					cout << "Take your " << localScore << " point!!!" << endl;
+
+					testWrite(playerName, localScore);
 					reset();
-					write();
+					read();
 					break;
 				}
 				else if (
@@ -131,47 +203,114 @@ void write()
 					(isvalid(i, arr[i]) && (arr[i] == arr[0] && arr[i] == arr[4] && arr[i] == arr[8])) ||
 					(isvalid(i, arr[i]) && (arr[i] == arr[2] && arr[i] == arr[5] && arr[i] == arr[8])))
 				{
-					cout << "Победа<<<< 8";
 					display();
+				
+					cout << "\n\n" << playerName << " - Winner !!!" << endl;
+					cout << "Take your " << localScore << " point!!!" << endl;
+
+					testWrite(playerName, localScore);
 					reset();
-					write();
+					read();
 					break;
 				}
 			}
 		} else {
-			cout << "Неверное значение...";
+			cout << "Wrong cahracter...";
 		}
 	}
 }
 void reset() {
-	int counter = 0;
-	cout << "\n\n" << ">>>>>Новая игра";
+	cout << "\n\n" << ">>>>>New game";
 	for (int i = 0; i < 9; ++i) {
-		counter++;
-		arr[i] = 0;
+		arr[i] = ' ';
 	}
 }
 
 void display() {
-	int counter = 0;
 
 	cout << "\n___________\n";
 	for (int i = 0; i < 9; ++i) {
-		++counter;
 		cout << "[" << arr[i] << "] ";
 		
-		if (counter == 3 || counter == 6) {
+		if (i == 2 || i == 5) {
 			cout << '\n';
 		}
 	}
 }
 
+void win(int i, string name, string score)
+{
+	cout << '\n' << arr[i] << " - Winner !!!";
+
+}
+
+
+void testWrite(string name, string score) 
+{
+		for (int i = 0;; ++i) {
+		
+			if ((scoreTable[i][0] == name)) {
+				int pScore, newScore;
+				string strScore;
+
+				pScore = atoi(scoreTable[i][1].c_str());
+				newScore = atoi(score.c_str());
+				pScore += newScore;
+				strScore = to_string(pScore);
+				scoreTable[i][1] = strScore;
+				showScore();
+				break;
+			}
+			else if ((scoreTable[i][0] == "")) {
+				score = "0";
+				scoreTable[i][0] = name;
+				scoreTable[i][1] = score;
+
+				showScore();
+				break;
+			}
+			else if ((scoreTable[i][0] != "")){
+			}
+		}
+}
+
+void showScore() // показать массив
+{
+		cout << "\n|_Score bar_|\n";
+		cout << scoreTable[0][0] << " - " << scoreTable[0][1] << endl;
+		cout << scoreTable[1][0] << " - " << scoreTable[1][1] << endl;
+		cout << scoreTable[2][0] << " - " << scoreTable[2][1] << endl;
+		cout << "\n______________\n";
+	
+}
+
+bool isGameStarted()
+{
+	return gameStarted;
+}
+
+void gameStart()
+{
+	gameStarted = true;
+}
+
+void gameStop()
+{
+	gameStarted = false;
+}
+
 int main()
 {
-    int a = 5;
-	int b = 5;
+	system("chcp 1251>nul");
 
-	write();
+	string a, b;
+
+	cout << "You name..." << endl;
+	cin >> a;
+	playerName = a;
+	testWrite(a, b);
+	reset();
+	read();
 
 	return 0;
 }
